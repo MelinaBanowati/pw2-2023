0d52805bece64894cf455fcd1f6af21c2b4fc4ce
MelinaBanowatiSI02 <146049929+MelinaBanowati@users.noreply.github.com>
2024-03-12 22:31:55 +0700

    Add files via upload

---
+++ pertemuan_3/tugas/home.php
@@ -0,0 +1,19 @@
+<?php
+    include_once 'header.php';
+?>
+
+<body>
+    <div class="container mt-5">
+        <header role="banner">
+            <h2>Student Activity Score - STTNF</h2>
+            <a href="">Home</a> | <a href="">Form Nilai</a> | <a href="">Data Nilai</a> | <a href="">Login</a>
+            <hr />
+        </header>
+				<h1>Welcome Home!</h1>
+				<p>Melina putri Banowati-0110123042-si02</p>
+	
+</body>
+
+<?php
+    include_once 'footer.php';
+?>
